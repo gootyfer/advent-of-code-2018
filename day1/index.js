20bf2d8a5965b2444307ed26c7f663929fc2465e
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-12-02 22:13:27 +0100

    Update day 1 solution to take input as is

--- day1/index.js
+++ day1/index.js
@@ -1,42 +1,37 @@
 const fs = require('fs');
 
 const results = [];
-const process = (numbers, prevResult) => numbers.reduce((acc, curr) =>{
-  const nextVal = acc.sum + curr;
-  const found = acc.freqs.find(el => el === nextVal);
-  
-  if(found) results.push(found);
-  return {
-    freqs: [...acc.freqs, nextVal],
-    sum: nextVal
-  }
-}, prevResult);
+const process = (numbers, prevResult) =>
+  numbers.reduce((acc, curr) => {
+    const nextVal = acc.sum + curr;
+    const found = acc.freqs.find(el => el === nextVal);
+
+    if (found) results.push(found);
+    return {
+      freqs: [...acc.freqs, nextVal],
+      sum: nextVal,
+    };
+  }, prevResult);
 
 fs.readFile('input.txt', 'utf8', (err, data) => {
-
   const numbers = data
+    .trim()
     .split('\n')
     .map(number => parseInt(number));
 
-  
-  
-  
-  const result = numbers.reduce((acc,curr) => acc + curr)
-  
+  const result = numbers.reduce((acc, curr) => acc + curr);
+
   console.log(`The final frequency is ${result}`);
 
-  
   let processingResults = {
     freqs: [],
-    sum: 0
+    sum: 0,
   };
-  while(results[0] === undefined){
+  while (results[0] === undefined) {
     processingResults = process(numbers, processingResults);
     //console.log(processingResults.freqs.filter(n => n > 10000));
     console.log(processingResults.sum, processingResults.freqs.length);
   }
 
   console.log(`The first freq that repeats twice is ${results[0]}`);
-
 });
-
